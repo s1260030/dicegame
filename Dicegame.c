0ddb894ea2ca9c7f25bd1038197c7013489b0080
Taiga Komatsu <s1260030@std1dc24.u-aizu.ac.jp>
2019-07-05 11:57:36 +0900

    Initial revision

---
+++ Dicegame.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+  srand((unsigned int)time(NULL));
+  
+  int d1 = rand() % 6 + 1;
+  int d2 = rand() % 6 + 1;
+  printf("Rolling the dice...\n");
+  printf("Die 1: %d \n",d1);
+  printf("Die 2: %d \n",d2);
+  printf("Total value: %d",d1+d2);
+  return 0;
+}
