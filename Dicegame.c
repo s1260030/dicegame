ebb1bb4484441e54932da401c17ee6c88591d16c
s1260030 <s1260030@std6dc38.u-aizu.ac.jp>
2019-07-06 14:24:25 +0900

    add won or lost

--- Dicegame.c
+++ Dicegame.c
@@ -11,12 +11,19 @@ int main(){
   printf("Hello, %s !\n",name);
 
   
+  
   int d1 = rand() % 6 + 1;
   int d2 = rand() % 6 + 1;
   printf("Rolling the dice...\n");
   printf("Die 1: %d \n",d1);
   printf("Die 2: %d \n",d2);
   printf("Total value: %d",d1+d2);
+  if((d1 + d2) > 7){
+    printf("You won\n");
+  }
+  else{
+    printf("You lost\n");
+  }
  
   return 0;
 }
