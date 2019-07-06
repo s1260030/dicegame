ed0ce0fba5d18901f1c84ad4150afd9c02100fba
s1260030 <s1260030@std6dc38.u-aizu.ac.jp>
2019-07-06 14:04:43 +0900

    add won lost massage

--- Dicegame.c
+++ Dicegame.c
@@ -5,6 +5,13 @@
 
 int main(){
   srand((unsigned int)time(NULL));
+  string name;
+  printf("What is your name?");
+  scanf("%s",name);
+  printf("Hello, %s !\n",name);
+
+  printf("Rollong the dice...\n");
+  
   
   int d1 = rand() % 6 + 1;
   int d2 = rand() % 6 + 1;
@@ -12,5 +19,11 @@ int main(){
   printf("Die 1: %d \n",d1);
   printf("Die 2: %d \n",d2);
   printf("Total value: %d",d1+d2);
+  if((d1 + d2) > 7){
+    printf("You won\n");
+  }
+  else{
+    printf("You lost!\n");
+  }
   return 0;
 }
